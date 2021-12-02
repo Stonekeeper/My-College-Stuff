bad92a4cd2eaea3dc1cb235a1de2f2fee32bf96c
Praveen Kumar S <55323914+Stonekeeper@users.noreply.github.com>
2021-12-02 11:44:44 +0530

    Add files via upload

---
+++ 5TH_SEM/IOT_Programs/2. Traffic.ino
@@ -0,0 +1,29 @@
+int red=13;
+int yellow=12;
+int green=11;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(red,HIGH);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,LOW);
+  delay(2000);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  digitalWrite(green,LOW);
+  delay(2000);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,HIGH);
+  delay(2000);
+  
+
+}
